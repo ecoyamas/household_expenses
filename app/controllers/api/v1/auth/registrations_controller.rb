2bb9bc5bb7f13482b57618827ee190ddf458a17c
ecoyamas <hayato53pu@gmail.com>
2021-03-20 10:41:12 +0900

    devise系のAPI設定

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,2 @@
+class API::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    mount_devise_token_auth_for "User", at: "auth"
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
   end
   root to: 'dashboard#index'
   resources :incomes
